e5b1e6490b6533c5c90175537615dce4add1f904
SAM0619TJ <genshin2006p@outlook.com>
2025-09-06 22:08:46 +0800

    作业框架

---
+++ hw1/include/Vector.h
@@ -0,0 +1,12 @@
+//
+// Created by Hacker on 25-9-6.
+//
+
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <vector>
+using std::size_t;
+namespace algebra{
+    using Vector = std::vector<double>;
+}
+#endif //VECTOR_H
---
+++ hw1/src/Vector.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Hacker on 25-9-6.
+#include "Vector.h"
+using namespace algebra;
